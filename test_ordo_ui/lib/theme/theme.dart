ab3d4e4aee0dce21682e1bfbf88e41801070d0de
fredi <fredibagusk@gmail.com>
2021-12-02 19:11:46 +0700

    install theme

---
+++ test_ordo_ui/lib/theme/theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBagroundColor = const Color(0xffFCF8F8);
+Color kPrimaryColor = const Color(0xff3C7DD9);
+Color kYellowColor = const Color(0xffDFAE1D);
+Color kRedColor = const Color(0xffFF485A);
+Color kblueColor = const Color(0xff64A1F4);
+Color kGreenColor = const Color(0xff47623F);
+
+Color kBlackColor = const Color(0xff000000);
+
+Color kGreyColor = const Color(0xffB4BBC6);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
